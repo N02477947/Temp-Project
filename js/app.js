40cc55e456e3f98f650e332d275c824c0a069a51
ws-degroate <david.degroate@whitestratus.com>
2014-07-15 11:21:05 -0400

    Index and App.js

---
+++ js/app.js
@@ -0,0 +1,79 @@
+'use strict';
+
+//=======================================
+// TOP LEVEL APPS
+//=======================================
+(function() {
+
+// Declare all app-defined dependencies/components
+var app = angular.module('cmsTestApp', [
+	/*'ngRoute',
+	'ngStorage',
+	'ui.bootstrap'*/
+]);
+
+// Configure page.
+app.config([/*'$routeProvider',*/ function(/*$routeProvider*/) {
+	console.log("CMS Test App is being configured.");
+	/*
+	 * CMS TEMPLATE CONFIGURATION
+	 */
+	var CMS_CONFIG = null;
+	$.ajax({
+		'async': false,
+		'global': false,
+		'url': "home.json",
+		'dataType': "json",
+		'success': function (data) {
+			CMS_CONFIG = data;
+			console.log("CMS data loaded: ", CMS_CONFIG);
+			/*for (var i=0; i<CMS_CONFIG.pages.length; i++) {
+				console.log("Compiling CMS page: ", CMS_CONFIG.pages[i]);
+				var templ = top;
+				for (var j=0; j<CMS_CONFIG.pages[i].content.length; j++){
+					console.log("Adding module: ", CMS_CONFIG.pages[i].content[j].module);
+					templ = templ+"<"+CMS_CONFIG.pages[i].content[j].module;
+					if (CMS_CONFIG.pages[i].content[j].parameters) {
+						//appending parameters to html
+						$.each(CMS_CONFIG.pages[i].content[j].parameters, function(key, value) {
+							templ = templ+" "+key+"=\""+value+"\"" ;
+							console.log("Parameter added: "+key+"="+value);
+						});
+					}
+					templ = templ+"> </"+CMS_CONFIG.pages[i].content[j].module+">";
+				}
+				console.log("Template created: ", templ);
+				$routeProvider.when('/'+CMS_CONFIG.pages[i].title, {
+					template: templ
+				});            	
+			}*/
+		}
+	});
+
+	/*
+	 * ROUTING
+	 */
+
+	/*$routeProvider.when('/home', {
+		template: '<div></div>'
+	});
+
+	$routeProvider.when('/', {
+		redirectTo: '/home'
+	});
+
+	$routeProvider.otherwise({redirectTo: '/home'});*/
+
+}]);
+
+app.run(['$rootScope', function($rootScope) {
+	console.log("CMS Test App is running.");
+	// ...
+}]);
+
+app.controller('cmsTestController', ['$scope', function($scope) {
+	console.log("CMS Test App controller is active.");
+	// ...
+}]);
+
+})();
